9a628bc3d033bb6948a0e75154da540a1f4bfe21
Tope <tope.ajibuwa@supermartng.com>
2020-05-14 22:53:58 +0100

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,569 @@
+export default {
+  items: [
+
+    {
+      title: true,
+      name: 'Admin Dashboard',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: 'text-center'             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Setup',
+      icon: 'icon-settings',
+      children: [
+        {
+          name: 'Operator',
+          url: '/operator',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Mode',
+          url: '/base/cards',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+
+        },
+        {
+          name: 'Zone',
+          url: '/base/carousels',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Area',
+          url: '/base/collapses',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Routes',
+          url: '/base/dropdowns',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Bus Stop',
+          url: '/base/forms',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Geo Fences',
+          url: '/base/jumbotrons',
+          // icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Analytics',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'Modal',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Revenue',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Operator',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Users',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'All',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Active',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Inactive',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Drivers',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'All',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Active',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Inactive',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Pending',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Bus Assistance',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'All',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Active',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Inactive',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Pending',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Trips',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'Waiting',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Completed',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Current',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Canceled',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Payments',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'Successful',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Unsuccessful',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Refunds',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Promo',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+      ],
+    },
+    {
+      name: 'Ratings',
+      icon: 'icon-user',
+      children: [
+        {
+          name: 'Driver',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        },
+        {
+          name: 'Bus',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+          attributes: {
+            style:{background: 'gray'}
+          }
+        }
+      ],
+    },
+    // {
+    //   name: 'Colors',
+    //   url: '/theme/colors',
+    //   icon: 'icon-drop',
+    // },
+    // {
+    //   name: 'Typography',
+    //   url: '/theme/typography',
+    //   icon: 'icon-pencil',
+    // },
+    // {
+    //   title: true,
+    //   name: 'Components',
+    //   wrapper: {
+    //     element: '',
+    //     attributes: {},
+    //   },
+    // },
+    // {
+    //   name: 'Base',
+    //   url: '/base',
+    //   icon: 'icon-puzzle',
+    //   children: [
+    //     {
+    //       name: 'Breadcrumbs',
+    //       url: '/base/breadcrumbs',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Cards',
+    //       url: '/base/cards',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Carousels',
+    //       url: '/base/carousels',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Collapses',
+    //       url: '/base/collapses',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Dropdowns',
+    //       url: '/base/dropdowns',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Forms',
+    //       url: '/base/forms',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Jumbotrons',
+    //       url: '/base/jumbotrons',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'List groups',
+    //       url: '/base/list-groups',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Navs',
+    //       url: '/base/navs',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Paginations',
+    //       url: '/base/paginations',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Popovers',
+    //       url: '/base/popovers',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Progress Bar',
+    //       url: '/base/progress-bar',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Switches',
+    //       url: '/base/switches',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Tables',
+    //       url: '/base/tables',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Tabs',
+    //       url: '/base/tabs',
+    //       icon: 'icon-puzzle',
+    //     },
+    //     {
+    //       name: 'Tooltips',
+    //       url: '/base/tooltips',
+    //       icon: 'icon-puzzle',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Buttons',
+    //   url: '/buttons',
+    //   icon: 'icon-cursor',
+    //   children: [
+    //     {
+    //       name: 'Buttons',
+    //       url: '/buttons/buttons',
+    //       icon: 'icon-cursor',
+    //     },
+    //     {
+    //       name: 'Button dropdowns',
+    //       url: '/buttons/button-dropdowns',
+    //       icon: 'icon-cursor',
+    //     },
+    //     {
+    //       name: 'Button groups',
+    //       url: '/buttons/button-groups',
+    //       icon: 'icon-cursor',
+    //     },
+    //     {
+    //       name: 'Brand Buttons',
+    //       url: '/buttons/brand-buttons',
+    //       icon: 'icon-cursor',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Charts',
+    //   url: '/charts',
+    //   icon: 'icon-pie-chart',
+    // },
+    // {
+    //   name: 'Icons',
+    //   url: '/icons',
+    //   icon: 'icon-star',
+    //   children: [
+    //     {
+    //       name: 'CoreUI Icons',
+    //       url: '/icons/coreui-icons',
+    //       icon: 'icon-star',
+    //       badge: {
+    //         variant: 'info',
+    //         text: 'NEW',
+    //       },
+    //     },
+    //     {
+    //       name: 'Flags',
+    //       url: '/icons/flags',
+    //       icon: 'icon-star',
+    //     },
+    //     {
+    //       name: 'Font Awesome',
+    //       url: '/icons/font-awesome',
+    //       icon: 'icon-star',
+    //       badge: {
+    //         variant: 'secondary',
+    //         text: '4.7',
+    //       },
+    //     },
+    //     {
+    //       name: 'Simple Line Icons',
+    //       url: '/icons/simple-line-icons',
+    //       icon: 'icon-star',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Notifications',
+    //   url: '/notifications',
+    //   icon: 'icon-bell',
+    //   children: [
+    //     {
+    //       name: 'Alerts',
+    //       url: '/notifications/alerts',
+    //       icon: 'icon-bell',
+    //     },
+    //     {
+    //       name: 'Badges',
+    //       url: '/notifications/badges',
+    //       icon: 'icon-bell',
+    //     },
+    //     {
+    //       name: 'Modals',
+    //       url: '/notifications/modals',
+    //       icon: 'icon-bell',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Widgets',
+    //   url: '/widgets',
+    //   icon: 'icon-calculator',
+    //   badge: {
+    //     variant: 'info',
+    //     text: 'NEW',
+    //   },
+    // },
+    // {
+    //   divider: true,
+    // },
+    // {
+    //   title: true,
+    //   name: 'Extras',
+    // },
+    // {
+    //   name: 'Pages',
+    //   url: '/pages',
+    //   icon: 'icon-star',
+    //   children: [
+    //     {
+    //       name: 'Login',
+    //       url: '/login',
+    //       icon: 'icon-star',
+    //     },
+    //     {
+    //       name: 'Register',
+    //       url: '/register',
+    //       icon: 'icon-star',
+    //     },
+    //     {
+    //       name: 'Error 404',
+    //       url: '/404',
+    //       icon: 'icon-star',
+    //     },
+    //     {
+    //       name: 'Error 500',
+    //       url: '/500',
+    //       icon: 'icon-star',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Disabled',
+    //   url: '/dashboard',
+    //   icon: 'icon-ban',
+    //   attributes: { disabled: true },
+    // },
+    // {
+    //   name: 'Download CoreUI',
+    //   url: 'https://coreui.io/react/',
+    //   icon: 'icon-cloud-download',
+    //   class: 'mt-auto',
+    //   variant: 'success',
+    //   attributes: { target: '_blank', rel: "noopener" },
+    // },
+    // {
+    //   name: 'Try CoreUI PRO',
+    //   url: 'https://coreui.io/pro/react/',
+    //   icon: 'icon-layers',
+    //   variant: 'danger',
+    //   attributes: { target: '_blank', rel: "noopener" },
+    // },
+  ],
+};
---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="6">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <Form>
+                      <h1>Login</h1>
+                      <p className="text-muted">Sign In to your account</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Username" autoComplete="username" />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                          <Button color="primary" className="px-4">Login</Button>
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">Forgot password?</Button>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col xs="12" className="text-center">
+                          Don't have an account?
+                          <Link to="/register">
+                            <Button color="link" className="px-0">Register</Button>
+                          </Link>
+                        </Col>
+
+                      </Row>
+                    </Form>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;
